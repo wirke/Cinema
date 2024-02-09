6a6fec65b59b3cd763e73924feb498915c5d5763
kristinastev <149689425+kristinastev@users.noreply.github.com>
2024-02-09 23:46:08 +0100

    1.0.1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar msg="Tekst za navbar"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Navbar',
   props: {
     msg: String
   }
